e6a26a1f89d4ce75bb0da5a52f70332cce18bb55
Gary <36412918+a2110560@users.noreply.github.com>
2020-09-05 14:12:13 +0800

    init

---
+++ app/src/main/java/com/example/myio/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.myio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void test1(View view) {
+    }
+
+    public void test2(View view) {
+    }
+
+    public void test3(View view) {
+    }
+
+    public void test4(View view) {
+    }
+
+    public void test5(View view) {
+    }
+
+    public void test6(View view) {
+    }
+}
